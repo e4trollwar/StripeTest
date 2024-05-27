f045aaa560d0ff8de212ae405528c8885b4e3a5d
unknown <angeloarimas@gmail.com>
2024-05-27 15:29:42 +0800

    asd

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -15,18 +15,13 @@ class paymentController extends Controller
 
         $signingSecret = 'sk_test_UVzt5mdJkYWRZxK1C7YimXaV';
         $header_signature = $request->header();
-        $header_te =preg_split("/,/",$header_signature);
-        $header_te_res = $header_te[1];
-        $header_te_res_one =preg_split("/=/",$header_te_res);
-        $header_te_res_final_res = $header_te_res_one[1];
-
 
         $payload= $request->getContent();
         
         $computedSignature = hash_hmac('sha256', $payload, $signingSecret);
 
 
-        $signature = hash_equals($header_te_res, $computedSignature);
+        $signature = hash_equals($header_signature, $computedSignature);
         if($signature){
             WebhookCall::insert([
               'payload' =>$payload,
