959dd007b563a7b91a1bcf4f5bf98babea43c849
unknown <angeloarimas@gmail.com>
2024-05-27 10:41:16 +0800

    asd

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -10,14 +10,14 @@ class paymentController extends Controller
 
     public function test(Request $request){
 
-        $signingSecret = 'sk_test_UVzt5mdJkYWRZxK1C7YimXaV';
+        $signingSecret = '1k_test_UVzt5mdJkYWRZxK1C7YimXaV';
         $header_signature = $request->header('Paymongo-Signature');
         $payload= $request->getContent();
         
         $computedSignature = hash_hmac('sha256', $payload, $signingSecret);
         $signature = hash_equals($header_signature, $computedSignature);
         WebhookCall::insert([
-          'payload' =>$computedSignature,
+          'payload' =>$signature,
         ]);
 
 
