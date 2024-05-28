8990b4bad1715685462072d093f324a6ab8c49d7
unknown <angeloarimas@gmail.com>
2024-05-28 11:50:37 +0800

    asd

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -32,10 +32,10 @@ class paymentController extends Controller
         
         //concatinated the time and $request->getContent() ex:1716800978{json response data} and the second value is my secret key ex: sk_test_UVzt5mdhyRgoZxK1C7YimXaV
 
-        $computedSignature = hash_hmac('sha256',$own_signature,$header_time_no_equal_res.$signingSecret);
+        $computedSignature = hash_hmac('sha256',$own_signature, $signingSecret);
 
 
-        $signature = hash_equals($computedSignature,$header_string_no_equal_res);
+        $signature = hash_equals($computedSignature,$header_time_no_equal_res.$header_string_no_equal_res);
         if($signature == 1 || $signature == true){
             WebhookCall::insert([
               'payload' =>'valid',
